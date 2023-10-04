a4303c4ef88e55bbc99005078d3300cc3d850033
Nguyễn Xuân Luân <nguyenxuan.luan@jonckers.com>
2023-10-04 14:16:34 +0700

    modify composer.json

--- src/Core/Installer.php
+++ src/Core/Installer.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace WordsOnline\TranslationManager\Core;
+
 use \OxidEsales\Eshop\Core\DbMetaDataHandler;
 
-final class Installer
+ class Installer
 {
-    public static function onActivate():void {
+    public static function onActivate() {
 
     }
-    public static function onDeactivate(): void {
+    public static function onDeactivate() {
  
     }
 }
