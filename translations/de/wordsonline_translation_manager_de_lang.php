145e8a431bb4d509f4d4e0eb6fc1bfa8c96b3a2c
Nguyễn Xuân Luân <nguyenxuan.luan@jonckers.com>
2023-10-06 15:49:29 +0700

    update translation

--- translations/de/wordsonline_translation_manager_de_lang.php
+++ translations/de/wordsonline_translation_manager_de_lang.php
@@ -5,6 +5,10 @@ $sLangName  = "Deutsch";
 // -------------------------------
 $aLang = [
     'charset'                          => 'UTF-8',
+    'SHOP_MODULE_GROUP_SETTINGS' => 'Einstellungen',
+    'SHOP_MODULE_wo_project_id' => 'Projekt-ID',
+    'SHOP_MODULE_wo_username' => 'Nutzername',
+    'SHOP_MODULE_wo_password'  => 'Passwort',
     'ALBANIAN'                         => 'Albanisch',
     'ARABIC'                           => 'Arabisch',
     'ARABIC_AE'                        => 'Arabic (AE)',
--- translations/en/wordsonline_translation_manager_en_lang.php
+++ translations/en/wordsonline_translation_manager_en_lang.php
@@ -5,6 +5,10 @@ $sLangName  = "Englisch";
 // -------------------------------
 $aLang = [
     'charset'                          => 'UTF-8',
+    'SHOP_MODULE_GROUP_SETTINGS' => 'Settings',
+    'SHOP_MODULE_wo_project_id' => 'Project id',
+    'SHOP_MODULE_wo_username' => 'Username',
+    'SHOP_MODULE_wo_password'  => 'Password',
     'ALBANIAN'                         => 'Albanian',
     'ARABIC'                           => 'Arabic',
     'ARABIC_AE'                        => 'Arabic (AE)',
