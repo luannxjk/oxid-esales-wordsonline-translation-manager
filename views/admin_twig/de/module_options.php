d4d5e11cec8b46d75ea6e2f42bf21e4f3c7b39aa
Nguyễn Xuân Luân <nguyenxuan.luan@jonckers.com>
2023-10-06 15:34:40 +0700

    add module option

---
+++ views/admin_twig/de/module_options.php
@@ -0,0 +1,8 @@
+<?php
+
+$aLang = [
+    'SHOP_MODULE_GROUP_SETTINGS' => 'Einstellungen',
+    'SHOP_MODULE_wo_project_id' => 'Projekt-ID',
+    'SHOP_MODULE_wo_username' => 'Nutzername',
+    'SHOP_MODULE_wo_password'  => 'Passwort'
+];
